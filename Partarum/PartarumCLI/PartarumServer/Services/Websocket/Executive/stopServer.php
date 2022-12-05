25ce025366997ffd1700a310b8142b629d724352
svenskanda <email@partarum.de>
2022-12-05 02:49:33 +0000

    0.0.1 - first round - delete

--- Partarum/PartarumCLI/PartarumServer/Services/Websocket/Executive/stopServer.php
+++
@@ -1,6 +0,0 @@
-<?php
-/*
-    ! Screen - Session nicht killen, bevor nicht der Server platt gemacht wurde
-
-    ! das gleiche wie disconnectServer.php
- */
--- Partarum/PartarumCLI/System/Filesystem/testEnum.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once "FolderPath.php";
-
-use Partarum\PartarumCLI\System\Filesystem\FolderPath;
-
-var_dump(FolderPath::get("API"));
